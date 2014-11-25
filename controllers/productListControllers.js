0064563fcf1463f577a1ef07dbd947ee154fc7c1
heynicecode <rickymuldrew@gmail.com>
2014-11-24 20:46:13 -0500

    create behavior for selected category class

--- controllers/productListControllers.js
+++ controllers/productListControllers.js
@@ -1,11 +1,15 @@
-angular.module( "sportsStore" )
-    .controller( "productListCtrl", function( $scope, $filter ) {
+angular.module("sportsStore")
+    .constant("productListActiveClass", "btn-primary")
+    .controller("productListCtrl", function($scope, $filter, productListActiveClass) {
         var selectedCategory = null;
 
-        $scope.selectCategory = function( newCategory ) {
+        $scope.selectCategory = function(newCategory) {
             selectedCategory = newCategory;
         }
-        $scope.categoryFilterFn = function( product ) {
+        $scope.categoryFilterFn = function(product) {
             return selectedCategory == null || product.category == selectedCategory;
+        }
+        $scope.getCategoryClass = function(category) {
+            return selectedCategory == category ? productListActiveClass : null;
         }
     });
