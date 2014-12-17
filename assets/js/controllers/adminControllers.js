282fe1a7397795efd638ffae7a4ca82b2a36de11
heynicecode <rickymuldrew@gmail.com>
2014-12-17 17:36:43 -0500

    Complete admin > orders view

--- assets/js/controllers/adminControllers.js
+++ assets/js/controllers/adminControllers.js
@@ -1,5 +1,6 @@
 angular.module("sportsStoreAdmin")
     .constant("authUrl", "http://localhost:2403/users/login")
+    .constant("ordersUrl", "http://localhost:2403/orders")
     .controller("authCtrl", function($scope, $http, $location, authUrl) {
 
         $scope.authenticate = function(user, pass) {
@@ -26,4 +27,28 @@ angular.module("sportsStoreAdmin")
         $scope.getScreen = function() {
             return $scope.current == "Products" ? "views/adminProducts.html" : "views/adminOrders.html";
         };
+    })
+    .controller("ordersCtrl", function($scope, $http, ordersUrl) {
+        $http.get(ordersUrl, {
+            withCredentials: true
+        })
+        .success(function(data) {
+            $scope.orders = data;
+        })
+        .error(function(error) {
+            $scope.error = error;
+        });
+        $scope.selectedOrder;
+
+        $scope.selectOrder = function(order) {
+            $scope.selectedOrder = order;
+        };
+        $scope.calcTotal = function(order) {
+            var total = 0;
+
+            for (var i = 0; i < order.products.length; i++) {
+                total += order.products[i].count * order.products[i].price;
+            }
+            return total;
+        }
     });
